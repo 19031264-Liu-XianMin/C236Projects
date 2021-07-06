512b937acbfdbb9c14582fc2f9c9ffde2b92ad85
Liu xianmin <1422195260@qq.com>
2021-07-06 21:52:22 +0800

    changes in different viewing for different users

--- FYPTesting/Models/Menu.cs
+++ FYPTesting/Models/Menu.cs
@@ -9,9 +9,48 @@ namespace FYPTesting.Models
         [Required(ErrorMessage = "Please enter Number")]
         public int Number { get; set; }
 
+        [Required(ErrorMessage = "Please enter Supplier Name")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Supplier Name 1-200 chars")]
+        public string SupplierName { get; set; }
+
+        [Required(ErrorMessage = "Please enter PO No")]
+        [StringLength(11, MinimumLength = 1, ErrorMessage = "PO No 1-11 chars")]
+        public String PONo { get; set; }
+
         [Required(ErrorMessage = "Please enter Date/Time")]
         [DataType(DataType.DateTime)]
         public DateTime OrderDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter Payment Terms")]
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "Payment Terms 1-10 chars")]
+        public string PaymentTerms { get; set; }
+
+        [Required(ErrorMessage = "Please enter Job Number")]
+        [StringLength(11, MinimumLength = 1, ErrorMessage = "Job Number 1-11 chars")]
+        public string JobNum { get; set; }
+
+        [Required(ErrorMessage = "Please enter Purchaser Name")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Supplier Name 1-200 chars")]
+        public string Purchaser { get; set; }
+
+        [Required(ErrorMessage = "Please enter Request Name")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Supplier Name 1-200 chars")]
+        public string Request { get; set; }
+
+        [Required(ErrorMessage = "Please enter PR No")]
+        [StringLength(11, MinimumLength = 1, ErrorMessage = "PR No 1-11 chars")]
+        public String PRNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter TSH PO Number")]
+        [StringLength(11, MinimumLength = 1, ErrorMessage = "TSH PO Number 1-11 chars")]
+        public string TSHPONO { get; set; }
+
+        [Required(ErrorMessage = "Please enter Part No")]
+        [StringLength(11, MinimumLength = 1, ErrorMessage = "Part No 1-11 chars")]
+        public String PartNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter Description")]
+        public String Description { get; set; }
 
         [Required(ErrorMessage = "Please enter Date/Time")]
         [DataType(DataType.DateTime)]
@@ -21,28 +60,6 @@ namespace FYPTesting.Models
         [DataType(DataType.DateTime)]
         public DateTime RevisedDelDate { get; set; }
 
-        [Required(ErrorMessage = "Please enter PO No")]
-        [StringLength(11, MinimumLength = 1, ErrorMessage = "PO No 1-11 chars")]
-        public String PONo { get; set; }
-
-        [Required(ErrorMessage = "Please enter PR No")]
-        [StringLength(11, MinimumLength = 1, ErrorMessage = "PR No 1-11 chars")]
-        public String PRNo { get; set; }
-
-        [Required(ErrorMessage = "Please enter Supplier Name")]
-        [StringLength(200, MinimumLength = 1, ErrorMessage = "Supplier Name 1-200 chars")]
-        public string SupplierName { get; set; }
-
-        [Required(ErrorMessage = "Please enter Payment Terms")]
-        [StringLength(10, MinimumLength = 1, ErrorMessage = "Payment Terms 1-10 chars")]
-        public string PaymentTerms { get; set; }
-
-        [Required(ErrorMessage = "Please enter Part No")]
-        public String PartNo { get; set; }
-
-        [Required(ErrorMessage = "Please enter Description")]
-        public String Description { get; set; }
-
         [Required(ErrorMessage = "Please enter Currency")]
         public string Currency { get; set; }
 
@@ -50,10 +67,17 @@ namespace FYPTesting.Models
         [Range(0.000, 9999999.000, ErrorMessage = "0-9999999")]
         public double Quantity { get; set; }
 
+        [Required(ErrorMessage = "Please enter Units of Measure")]
+        [Range(0.000, 9999999.000, ErrorMessage = "0-9999999")]
+        public double UOM { get; set; }
+
+        [Required(ErrorMessage = "Please enter Unit Price")]
+        [Range(0.000, 9999999.000, ErrorMessage = "0-9999999")]
+        public double UnitPrice { get; set; }
+
         [Required(ErrorMessage = "Please enter Original Amount")]
         [Range(0.00, 9999999.00, ErrorMessage = "0-9999999 marks")]
         public double OrigAmt { get; set; }
-
 
     }
 }
