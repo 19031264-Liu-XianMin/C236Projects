f829efc420857d76d45b4007bdeae143d5efb9fe
Russell <19022963@LAPTOP-GCHRC6CI>
2021-07-25 16:01:20 +0800

    HotFix2.0

--- FYPTesting/Controllers/MenuController.cs
+++ FYPTesting/Controllers/MenuController.cs
@@ -215,7 +215,7 @@ namespace FYPTesting.Controllers
             else
             {
                 string insert = @"INSERT INTO Menu(OrderDate, DueDate, Purchaser, Request, SupplierName, PONo, TSHPONO, PRNo, PartNo, JobNum, Currency, Quantity, PaymentTerms, UOM, UnitPrice, OrigAmt, Description) VALUES
-                 ('{0:YYYY-MM-DD}', '{1:YYYY-MM-DD}', '{2}', '{3}', '{4}', '{5}', {6}, {7}, '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', '{14}', '{15}', '{16}')";
+                 ('{0:yyyy-MM-dd}', '{1:yyyy-MM-dd}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', {11}, '{12}', {13}, {14}, {15}, '{16}')";
                 if (DBUtl.ExecSQL(insert, mn.OrderDate, mn.DueDate, mn.RevisedDelDate, mn.Purchaser,mn.Request,mn.SupplierName, mn.PONo, mn.TSHPONO, mn.PRNo, mn.PartNo, mn.JobNum, mn.Currency, mn.Quantity, mn.PaymentTerms, mn.UOM, mn.OrigAmt, mn.Description) == 1)
                 {
                     TempData["Message"] = "Purchase Order Updated";
@@ -232,3 +232,7 @@ namespace FYPTesting.Controllers
 
     }
 }
+
+
+//                 (SupplierName, PONo, OrderDate, PaymentTerms, JobNum, Purchaser, Request, PRNo, TSHPONO, PartNo, Description, DueDate, Currency, Quantity, UOM, UnitPrice, OrigAmt) Values
+//                 ('{0}', '{1}', '{2:yyyy-MM-dd}', '{2}', '{2}', '{2}', '{2}', '{2}', '{2}', '{2}', '{2}', '{0:yyyy-MM-dd}', { 7}, { 7}, { 7}, { 7}, { 7},}'
