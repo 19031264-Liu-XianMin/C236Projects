8eae475f8ff92850369aaf697068c21b9b02ea91
Liu xianmin <1422195260@qq.com>
2021-07-25 20:37:35 +0800

    still got errors on addpo

--- FYPTesting/Controllers/MenuController.cs
+++ FYPTesting/Controllers/MenuController.cs
@@ -217,7 +217,7 @@ namespace FYPTesting.Controllers
             else
             {
                 string insert = @"INSERT INTO Menu(OrderDate, DueDate, Purchaser, Request, SupplierName, PONo, TSHPONO, PRNo, PartNo, JobNum, Currency, Quantity, PaymentTerms, UOM, UnitPrice, OrigAmt, Description) VALUES
-                 ('{0:yyyy-MM-dd}', '{1:yyyy-MM-dd}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', {11}, '{12}', {13}, {14}, {15}, '{16}')";
+                 ('{0:DD-MM-YYYY}', '{1:DD-MM-YYYY}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', {11}, '{12}', {13}, {14}, {15}, '{16}')";
                 if (DBUtl.ExecSQL(insert, mn.OrderDate, mn.DueDate, mn.RevisedDelDate, mn.Purchaser,mn.Request,mn.SupplierName, mn.PONo, mn.TSHPONO, mn.PRNo, mn.PartNo, mn.JobNum, mn.Currency, mn.Quantity, mn.PaymentTerms, mn.UOM, mn.OrigAmt, mn.Description) == 1)
                 {
                     TempData["Message"] = "Purchase Order Updated";
